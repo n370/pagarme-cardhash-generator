a6130395438b813a624378493702bdf3c76f6ff7
Dylson Valente Neto <ama@n370.info>
2018-03-27 16:39:14 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const inquirer = require('inquirer');
+const pagarme = require('pagarme');
+
+inquirer.prompt([
+    {
+        name: 'encryption_key',
+        message: 'Chave de encriptacao: '
+    },
+    {
+        name: 'card_number',
+        message: 'Nro do cartao: '
+    },
+    {
+        name: 'card_holder_name',
+        message: 'Nome no cartao: '
+    },
+    {
+        name: 'card_expiration_date',
+        message: 'Data de expiracao: '
+    },
+    {
+        name: 'card_cvv',
+        message: 'CVV: '
+    }
+])
+.then(answers => {
+    const card = {
+        card_number: answers.card_number,
+        card_holder_name: answers.card_holder_name,
+        card_expiration_date: answers.card_expiration_date,
+        card_cvv: answers.card_cvv,
+    };
+    return pagarme.client.connect({
+        encryption_key: answers.encryption_key
+    })
+    .then(client => client.security.encrypt(card));
+})
+.then(card_hash => console.log(card_hash));
